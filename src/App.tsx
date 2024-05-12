d38a126bc5911b045df7ffc2075c30e103b38989
apoorv1410 <bhattapoorv10@gmail.com>
2024-05-12 18:18:14 +0530

    #3 ADDED lexical text editor core

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import TextEditor from './components/TextEditor';
 
 function App() {
   return (
     <div className="App">
+      <TextEditor></TextEditor>
     </div>
   );
 }
